f6cd5e7c7212e7d3f5bc61a19f74609718b6e6c5
Vivek Nayyar <vivek.of.nayyar@gmail.com>
2024-06-26 20:20:59 +0530

    feat: added more examples

--- using-llm-with-langchain/2.js
+++ using-llm-with-langchain/2.js
@@ -1,1 +1,19 @@
-// Try the same with a different llm
+import { ChatOpenAI } from "@langchain/openai";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// we have connected to our gpt model
+const llm = new ChatOpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  model: "gpt-4",
+});
+
+// we have a question
+
+const question = "What is the capital of India?";
+// we are invoking the model
+
+const response = await llm.invoke(question);
+
+console.log(response.content);
---
+++ using-llm-with-langchain/3.js
@@ -0,0 +1,3 @@
+// try the same with claude and see if it works
+// try with mixtral
+// try with llama
