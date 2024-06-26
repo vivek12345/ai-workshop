46d2292e68ef65532b5b07bd785db677a0b82a51
Vivek Nayyar <vivek.of.nayyar@gmail.com>
2024-06-26 20:51:46 +0530

    feat: modified templates

--- prompt-templates/1.js
+++ prompt-templates/1.js
@@ -4,6 +4,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const chat = new ChatOpenAI({
+const llm = new ChatOpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+const promptTemplate = PromptTemplate.fromTemplate(
+  `What is the capital of {country}?`
+);
+
+const prompt = await promptTemplate.partial({ country: "India" });
+
+console.log(prompt);
+
+const response = await llm.invoke(prompt);
+
+console.log(response.content);
