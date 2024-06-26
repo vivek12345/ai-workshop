a94d2c0549eaa4233da64ef0bbc835c37b1bcd86
Vivek Nayyar <vivek.of.nayyar@gmail.com>
2024-06-26 18:42:38 +0530

    feat: added structured output

--- structured-output/1.js
+++ structured-output/1.js
@@ -1,5 +1,4 @@
 import { ChatOpenAI } from "@langchain/openai";
-import { z } from "zod";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -7,16 +6,3 @@ dotenv.config();
 const model = new ChatOpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-const joke = z.object({
-  setup: z.string().describe("The setup of the joke"),
-  punchline: z.string().describe("The punchline to the joke"),
-  rating: z.number().optional().describe("How funny the joke is, from 1 to 10"),
-});
-
-const jsonModel = model.withStructuredOutput(joke);
-
-const question = "Tell me a joke about cats";
-const response = await jsonModel.invoke(question);
-
-console.log(response);
